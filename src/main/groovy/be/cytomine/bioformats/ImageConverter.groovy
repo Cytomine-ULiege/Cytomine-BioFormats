a76fb062b06a4ad341d3316bcecf567e0e0fdf66
Ulysse Rubens <urubens@uliege.be>
2021-10-15 11:57:24 +0200

    Add missing reference info

--- src/main/groovy/be/cytomine/bioformats/ImageConverter.groovy
+++ src/main/groovy/be/cytomine/bioformats/ImageConverter.groovy
@@ -18,9 +18,35 @@
  * USA.
  */
 
+/*
+ * Cytomine-Bioformats, a wrapper to link Bio-formats with Cytomine.
+ * Copyright (C) 2015-2021 cytomine.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ *  your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ */
 
-
-
+/*
+ * This file is highly inspired from loci.formats.tools.ImageConverter.java:
+ * Bio-Formats command line tools for reading and converting files
+ * Copyright (C) 2005 - 2017 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * https://github.com/ome/bioformats/blob/develop/components/bio-formats-tools/src/loci/formats/tools/ImageConverter.java
+ */
 
 package be.cytomine.bioformats
 
